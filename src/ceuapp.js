7af8908317f6574654637eba772bbc7adc895362
myyusuf <myyusuf1911@gmail.com>
2017-06-17 15:56:17 +0700

    Init Workspace

--- src/ceuapp.js
+++ src/ceuapp.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter, Route } from 'react-router-dom';
+import Workspace from './Workspace.jsx';
+
+ReactDOM.render(
+  <HashRouter>
+    <Workspace>
+    </Workspace>
+  </HashRouter>,
+  document.getElementById('app')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,18 @@ var path = require('path');
 module.exports = {
   entry: './src/ceuapp.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public/javascripts'),
     filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ],
   }
 };
