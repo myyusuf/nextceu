aff482991fb7bc307592f479c33921db4c8e6669
myyusuf <myyusuf1911@gmail.com>
2017-06-30 21:12:16 +0700

    fix side menu

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import { Row, Col } from 'react-bootstrap';
+import { Grid, Row, Col } from 'react-bootstrap';
 
 
 class Dashboard extends React.Component {
@@ -34,6 +34,66 @@ class Dashboard extends React.Component {
             </Col>
           </Row>
         </div>
+        <Grid fluid>
+          <Row>
+              <Col xs={6} lg={3}>
+                  <div className="card">
+                      <div className="card-body pv">
+                          <div className="clearfix">
+                              <div className="pull-left">
+                                  <h4 className="m0 text-thin">350</h4><small className="m0 text-muted"><em className="mr-sm ion-arrow-up-b"></em>New visitors</small>
+                              </div>
+                              <div className="pull-right mt-lg">
+                                  <div id="sparkline2" data-line-color="#4caf50" className="sparkline"></div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </Col>
+              <Col xs={6} lg={3}>
+                  <div className="card">
+                      <div className="card-body pv">
+                          <div className="clearfix">
+                              <div className="pull-left">
+                                  <h4 className="m0 text-thin">10,200</h4><small className="m0 text-muted"><em className="mr-sm ion-arrow-down-b"></em>Page views</small>
+                              </div>
+                              <div className="pull-right mt-lg">
+                                  <div id="sparkline1" data-line-color="#03A9F4" className="sparkline"></div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </Col>
+              <Col xs={6} lg={3} className="visible-lg">
+                  <div className="card">
+                      <div className="card-body pv">
+                          <div className="clearfix">
+                              <div className="pull-left">
+                                  <h4 className="m0 text-thin">880</h4><small className="m0 text-muted"><em className="mr-sm ion-arrow-up-b"></em>Last income</small>
+                              </div>
+                              <div className="pull-right mt-lg">
+                                  <div id="sparkline3" data-line-color="#ab47bc" className="sparkline"></div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </Col>
+              <Col xs={6} lg={3} className="visible-lg">
+                  <div className="card">
+                      <div className="card-body pv">
+                          <div className="clearfix">
+                              <div className="pull-left">
+                                  <h4 className="m0 text-thin">780</h4><small className="m0 text-muted"><em className="mr-sm ion-arrow-up-b"></em>Reservations</small>
+                              </div>
+                              <div className="pull-right mt-lg">
+                                  <div id="sparkline4" data-line-color="#e91e63" className="sparkline"></div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </Col>
+          </Row>
+        </Grid>
       </section>
     );
   }
--- src/workspace/Sidebar.jsx
+++ src/workspace/Sidebar.jsx
@@ -20,26 +20,40 @@ class Sidebar extends React.Component {
             <aside className="sidebar-container">
               <div className="sidebar-header">
                   <div className="pull-right pt-lg text-muted hidden"><em className="ion-close-round"></em></div>
-                  <a href="#" className="sidebar-header-logo"><img src="images/logo.png" data-svg-replace="img/logo.svg" alt="Logo" /><span className="sidebar-header-logo-text">Centric</span></a>
+                  <a href="#" className="sidebar-header-logo"><img src="images/logo.png" data-svg-replace="img/logo.svg" alt="Logo" /><span className="sidebar-header-logo-text">CEU</span></a>
               </div>
               <div className="sidebar-content">
                   <div className="sidebar-toolbar text-center">
-                      <a href=""><img src="img/user/01.jpg" alt="Profile" className="img-circle thumb64" /></a>
+                      <a href=""><img src="images/user/01.jpg" alt="Profile" className="img-circle thumb64" /></a>
                       <div className="mt">Welcome, Willie Webb</div>
                   </div>
                   <nav className="sidebar-nav">
                       <ul>
-                          <li className="active">
-                            <span className="pull-right nav-label"><span className="badge bg-success">2</span></span><span className="nav-icon">
-                            <img src="" data-svg-replace="img/icons/aperture.svg" alt="MenuItem" className="hidden" /></span>
-                            <span>Dashboard</span>
+                        <li className="active">
+                          <a href="dashboard.html" className="ripple">
+                            <span className="pull-right nav-label">
+                              <span className="badge bg-success">2</span>
+                            </span>
+                            <span className="nav-icon">
+                              <img src="" data-svg-replace="img/icons/aperture.svg" alt="MenuItem" className="hidden" />
+                              </span>
+                              <span>
+                                Dashboard
+                              </span>
+                              <span className="md-ripple">
+                              </span>
+                            </a>
                           </li>
                           <li className="">
-                            <span className="pull-right nav-label"></span><span className="nav-icon">
-                            <img src="" data-svg-replace="img/icons/radio-waves.svg" alt="MenuItem" className="hidden" /></span>
-                            <span>Cards</span>
+                            <a href="cards.html" className="ripple">
+                              <span className="pull-right nav-label"></span>
+                              <span className="nav-icon">
+                                <img src="" data-svg-replace="img/icons/radio-waves.svg" alt="MenuItem" className="hidden" />
+                              </span>
+                              <span>Cards</span>
+                              <span className="md-ripple"></span>
+                            </a>
                           </li>
-
                       </ul>
                   </nav>
               </div>
