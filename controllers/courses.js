19c1c53d716e80e70d1b423336ae7806f49ccf0b
myyusuf <myyusuf1911@gmail.com>
2017-07-23 19:17:15 +0700

    Fix update course schedule

--- controllers/courses.js
+++ controllers/courses.js
@@ -64,6 +64,23 @@ exports.update = function(req, res, next) {
         .then((foundHospital) => {
           course.setHospital1(foundHospital)
           .then(() => {
+            course.planStartDate = req.body.planStartDate;
+            course.planStartDate1 = req.body.planStartDate1;
+            course.planStartDate2 = req.body.planStartDate2;
+            course.planStartDate3 = req.body.planStartDate3;
+            course.planEndDate = req.body.planEndDate;
+            course.planEndDate1 = req.body.planEndDate1;
+            course.planEndDate2 = req.body.planEndDate2;
+            course.planEndDate3 = req.body.planEndDate3;
+
+            course.realStartDate = req.body.realStartDate;
+            course.realStartDate1 = req.body.realStartDate1;
+            course.realStartDate2 = req.body.realStartDate2;
+            course.realStartDate3 = req.body.realStartDate3;
+            course.realEndDate = req.body.realEndDate;
+            course.realEndDate1 = req.body.realEndDate1;
+            course.realEndDate2 = req.body.realEndDate2;
+            course.realEndDate3 = req.body.realEndDate3;
 
             course.save()
             .then((result) => {
@@ -78,6 +95,23 @@ exports.update = function(req, res, next) {
       } else {
         course.setHospital1(null)
         .then(() => {
+          course.planStartDate = req.body.planStartDate;
+          course.planStartDate1 = req.body.planStartDate1;
+          course.planStartDate2 = req.body.planStartDate2;
+          course.planStartDate3 = req.body.planStartDate3;
+          course.planEndDate = req.body.planEndDate;
+          course.planEndDate1 = req.body.planEndDate1;
+          course.planEndDate2 = req.body.planEndDate2;
+          course.planEndDate3 = req.body.planEndDate3;
+
+          course.realStartDate = req.body.realStartDate;
+          course.realStartDate1 = req.body.realStartDate1;
+          course.realStartDate2 = req.body.realStartDate2;
+          course.realStartDate3 = req.body.realStartDate3;
+          course.realEndDate = req.body.realEndDate;
+          course.realEndDate1 = req.body.realEndDate1;
+          course.realEndDate2 = req.body.realEndDate2;
+          course.realEndDate3 = req.body.realEndDate3;
 
           course.save()
           .then((result) => {
