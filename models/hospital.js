2e27dd9c968525e4faad388bb8767ff708148a82
myyusuf <myyusuf1911@gmail.com>
2017-08-29 13:30:12 +0700

    Hospital create

--- models/hospital.js
+++ models/hospital.js
@@ -2,7 +2,8 @@
 module.exports = function(sequelize, DataTypes) {
   var Hospital = sequelize.define('Hospital', {
     code: DataTypes.STRING,
-    name: DataTypes.STRING
+    name: DataTypes.STRING,
+    hospitalType: DataTypes.INTEGER,
   }, {
     classMethods: {
       associate: function(models) {
