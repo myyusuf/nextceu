d5a701df17f41225e3a1d4064da5715545be3f1c
myyusuf <myyusuf1911@gmail.com>
2017-09-21 14:18:52 +0700

    Assistance

--- models/assistance.js
+++ models/assistance.js
@@ -3,7 +3,7 @@ module.exports = function(sequelize, DataTypes) {
   var Assistance = sequelize.define('Assistance', {
     code: DataTypes.STRING,
     name: DataTypes.STRING,
-    aventDate: DataTypes.DATEONLY,
+    eventDate: DataTypes.DATEONLY,
     description: DataTypes.STRING
   }, {
     classMethods: {
