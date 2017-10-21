0a2ff458ca913336566606304425da62d103a479
myyusuf <myyusuf1911@gmail.com>
2017-10-22 00:43:14 +0700

    Pengampu filters

--- controllers/pengampus.js
+++ controllers/pengampus.js
@@ -6,16 +6,24 @@ const sendError = (err, res) => {
 
 exports.findAll = function findAll(req, res) {
   const searchText = req.query.searchText ? `%${req.query.searchText}%` : '%%';
+  const departmentId = req.query.searchDepartment;
   const limit = req.query.pageSize ? parseInt(req.query.pageSize, 10) : 10;
   const currentPage = req.query.currentPage ? parseInt(req.query.currentPage, 10) : 1;
   const offset = (currentPage - 1) * limit;
+
+  const where = {
+    $or: [
+      { code: { $ilike: searchText } },
+      { name: { $ilike: searchText } },
+    ],
+  };
+
+  if (departmentId) {
+    where.DepartmentId = departmentId;
+  }
+
   models.Pengampu.findAndCountAll({
-    where: {
-      $or: [
-        { code: { $ilike: searchText } },
-        { name: { $ilike: searchText } },
-      ],
-    },
+    where,
     include: [
       { model: models.Department },
     ],
